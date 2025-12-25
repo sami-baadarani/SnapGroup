2601f85501037a795e4b5f7e4315ff85f249607b
Sami Baadarani <dev@samib.me>
2025-12-25 17:20:36 +1100

    Initial Commit

---
+++ SnapGroup/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SnapGroup
+//
+//  Created by Sami Baadarani on 25/12/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SnapGroup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SnapGroup
+//
+//  Created by Sami Baadarani on 25/12/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
